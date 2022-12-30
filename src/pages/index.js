44ce92fcb9afea0e32c1b2c4446830f4fd5cc28b
Kemin lau <keminlau@hotmail.com>
2022-12-30 22:34:35 +0800

    finished the index page UI prototype

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import PageLayout from "../components/PageLayout"
 import Seo from "../components/Seo"
 import { StaticImage } from "gatsby-plugin-image"
 import './index.css'
+import logo from "../images/man-logo.jpg"
 
 const IndexPage = () =>{
     
@@ -10,9 +11,9 @@ const IndexPage = () =>{
       <PageLayout>
         <div className="index container mx-auto px-4">
           <div className="herox flex flex-col md:flex-row">
-            <div className="hero w-full md:w-1/2">
-              <h1 className="headline mb-4 md:mx-4">你好！</h1>
-              <div className=" text-lg md:text-xl md:mx-4">
+            <div className="hero mt-10 w-full md:w-1/2">
+              <h1 className="headline mb-4 md:mx-4 text-5xl">你好，朋友！</h1>
+              <div className="description text-lg md:text-xl md:mx-4">
                 <p>我叫刘建文，这是我的个人博客（Nake 裸 Man 人）。</p>
                 <p>
                   我的职业是一名Web全栈开发者，但个人兴趣广泛，对哲学，历史，教育等都有相当的关注。<em>做研究</em>  是我最大的兴趣，此外，我还热爱运动，“运动书生”
@@ -32,8 +33,31 @@ const IndexPage = () =>{
             <div className="decoration w-full md:w-1/2 p-5 flex justify-center"><StaticImage src="../images/shiyao.png" layout="fixed" width={300} objectFit="fill" alt="留一个彩蛋，小女孩是一个豆瓣友邻的作品，她叫sy" ></StaticImage></div>
           </div>
           <section className="lastestpost">
-            <div className="heading">最新文章</div>
-
+            <h2 className="heading">
+              <div>
+                <div className="title">最新文章</div>              
+              </div>
+              <div className="">--</div>
+            </h2>
+            <PostList />
+          </section>
+          <section className="featuredpost">
+            <h2 className="heading">
+              <div>
+                <div className="title">推荐文章</div>              
+              </div>
+              <div className="">--</div>
+            </h2>
+            <FeaturedList />
+          </section>
+          <section className="projects">
+            <h2 className="heading">
+              <div>
+                <div className="title">公开项目</div>              
+              </div>
+              <div className="">--</div>
+            </h2>
+            <ProjectList />
           </section>
         </div>
       </PageLayout>
@@ -41,3 +65,70 @@ const IndexPage = () =>{
 }
 export const Head = () => <Seo title="INDEX" />
 export default IndexPage
+
+const PostList = ({}) => {
+  return ( 
+    <div className="postlist">
+      {
+        [1,2,3,4,5].map( (item) =>  <PostItem id={item} />)
+      } 
+    </div>
+   );
+}
+ 
+const PostItem = ({id}) => {
+  return ( 
+    <>
+      <a href="slug" className="post"><h3>{id}  文章正题有不少的字呢 a blog article with name! </h3>  <time >2022.12.12</time></a>
+      
+    </>
+   );
+}
+const FeaturedList = ({}) => {
+  return ( 
+    <div className="postlist grid  lg:grid-cols-2 xl:grid-cols-3">
+      {
+        [1,2,3,4,5,6].map( (item) =>  <PostCard id={item} />)
+      } 
+    </div>
+   );
+}
+ 
+const PostCard = ({id}) => {
+  return ( 
+    <div className="flex rounded bg-white border-[1px] p-4 m-1 relative">
+      <div className="img flex flex-col justify-center max-w-[50px]"><img src={logo} alt="" /> </div>
+      <div className="ml-5">
+        <time className="font-mono font-thin text-xs text-gray-500">2022.12.25 星期五</time>
+        <a href="slug" className="postcard text-xl font-normal"><h3>{id} a blog article with a looooooooong name! </h3> </a>
+      </div>
+      
+    </div>
+   );
+}
+
+const ProjectList = ({}) => {
+  return ( 
+    <div className="projectlist grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {
+        [1,2,3,4,5,6].map( (item) =>  <ProjectCard id={item} />)
+      } 
+    </div>
+   );
+}
+ 
+const ProjectCard = ({id}) => {
+  return ( 
+    <div className="flex flex-col rounded bg-gray-100 p-4 m-1">
+      <div className="ml-5">
+        <time className="font-mono font-thin text-xs text-gray-500">2022</time>
+        <a href="slug" className="prjectcard text-xl font-normal"><h3><span className="border rounded-full"> {id} </span>nakeman.cn 博客 </h3> </a>
+        <sub> 这是一个人个博客，写作大量文章，和技术介保姆</sub>
+      </div>
+      <div className="link ml-3 p-2 mx-3">
+        <a href="">Demo</a> <a href="">相关文章</a> <a href="">源码</a>
+      </div>
+      
+    </div>
+   );
+}
